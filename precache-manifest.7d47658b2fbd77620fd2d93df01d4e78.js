ad0d45d61669a93217d397c08bb597d159e5b231
unknown <ganesh.srambikal@gmail.com>
2020-01-16 19:34:50 +0530

    initial

---
+++ precache-manifest.7d47658b2fbd77620fd2d93df01d4e78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d53d81a95d9e8e55c9d4e0fc30301d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94a89a7e4c7ca2f9248a",
+    "url": "/static/css/main.827a75fe.chunk.css"
+  },
+  {
+    "revision": "5ed74d65e1a60c53f515",
+    "url": "/static/js/2.e2800190.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e2800190.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94a89a7e4c7ca2f9248a",
+    "url": "/static/js/main.09e372cc.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
